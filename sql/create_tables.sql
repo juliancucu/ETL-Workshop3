6a385a03f2a95751dac4d5c94ece76782b2f304c
juliancucu <julian_david.cuellar@uao.edu.co>
2026-05-20 15:22:38 -0500

    fix

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,4 @@
--- 1. Tabla Raw (Auditoría)
+-- Tabla Raw 
 CREATE TABLE IF NOT EXISTS raw_happiness_events (
     raw_event_id SERIAL PRIMARY KEY,
     kafka_offset BIGINT,
@@ -7,18 +7,18 @@ CREATE TABLE IF NOT EXISTS raw_happiness_events (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 2. Dimensiones
+--  Dimensiones
 CREATE TABLE IF NOT EXISTS dim_country (
     country_id SERIAL PRIMARY KEY,
-    country_name VARCHAR(100) UNIQUE NOT NULL -- ¡El UNIQUE es vital para tu ON CONFLICT!
+    country_name VARCHAR(100) UNIQUE NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS dim_date (
     date_id SERIAL PRIMARY KEY,
-    year INT UNIQUE NOT NULL -- ¡El UNIQUE es vital para tu ON CONFLICT!
+    year INT UNIQUE NOT NULL 
 );
 
--- 3. Tabla de Hechos
+-- Tabla de Hechos
 CREATE TABLE IF NOT EXISTS fact_predictions (
     prediction_id SERIAL PRIMARY KEY,
     raw_event_id INT REFERENCES raw_happiness_events(raw_event_id),
@@ -27,5 +27,5 @@ CREATE TABLE IF NOT EXISTS fact_predictions (
     actual_score NUMERIC(10, 4),
     predicted_score NUMERIC(10, 4),
     prediction_error NUMERIC(10, 4),
-    prediction_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- ¡Así cumple el requisito del PDF sin enviarlo desde Python!
+    prediction_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
 );
